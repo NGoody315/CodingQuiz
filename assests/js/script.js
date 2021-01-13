f72e61a88988dcb2ec7e14a15217fb447a107286
Nikki G <nikki.goodridge@gmail.com>
2021-01-13 14:48:26 -0500

    added score counter var

---
+++ assests/js/script.js
@@ -0,0 +1,11 @@
+//Assignments
+var buttonEl = document.getElementById("begin-quiz");
+var scoreCounter = 0;
+
+//Functions Here
+
+const questionOne = function(){
+
+}
+//Event Listeners
+buttonEl.addEventListener("click", questionOne)
